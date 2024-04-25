b6f9977194f1ab9d1c5e7e33ddf06a6c2c51ba04
Xinyu Chen <1546683151cxy@gmail.com>
2024-04-24 20:07:58 -0400

    add features

--- pas/src/pas/tetris/agents/TetrisQAgent.java
+++ pas/src/pas/tetris/agents/TetrisQAgent.java
@@ -1,6 +1,5 @@
 package src.pas.tetris.agents;
 
-
 // SYSTEM IMPORTS
 import java.util.Iterator;
 import java.util.List;
@@ -19,32 +18,30 @@ import edu.bu.tetris.nn.LossFunction;
 import edu.bu.tetris.nn.Optimizer;
 import edu.bu.tetris.nn.models.Sequential;
 import edu.bu.tetris.nn.layers.Dense; // fully connected layer
-import edu.bu.tetris.nn.layers.ReLU;  // some activations (below too)
+import edu.bu.tetris.nn.layers.ReLU; // some activations (below too)
 import edu.bu.tetris.nn.layers.Tanh;
 import edu.bu.tetris.nn.layers.Sigmoid;
 import edu.bu.tetris.training.data.Dataset;
 import edu.bu.tetris.utils.Pair;
 
-
 public class TetrisQAgent
-    extends QAgent
-{
+        extends QAgent {
 
     public static final double EXPLORATION_PROB = 0.05;
 
     private Random random;
 
-    public TetrisQAgent(String name)
-    {
+    public TetrisQAgent(String name) {
         super(name);
         this.random = new Random(12345); // optional to have a seed
     }
 
-    public Random getRandom() { return this.random; }
+    public Random getRandom() {
+        return this.random;
+    }
 
     @Override
-    public Model initQFunction()
-    {
+    public Model initQFunction() {
         // build a single-hidden-layer feedforward network
         // this example will create a 3-layer neural network (1 hidden layer)
         // in this example, the input to the neural network is the
@@ -62,62 +59,60 @@ public class TetrisQAgent
     }
 
     /**
-        This function is for you to figure out what your features
-        are. This should end up being a single row-vector, and the
-        dimensions should be what your qfunction is expecting.
-        One thing we can do is get the grayscale image
-        where squares in the image are 0.0 if unoccupied, 0.5 if
-        there is a "background" square (i.e. that square is occupied
-        but it is not the current piece being placed), and 1.0 for
-        any squares that the current piece is being considered for.
-        
-        We can then flatten this image to get a row-vector, but we
-        can do more than this! Try to be creative: how can you measure the
-        "state" of the game without relying on the pixels? If you were given
-        a tetris game midway through play, what properties would you look for?
+     * This function is for you to figure out what your features
+     * are. This should end up being a single row-vector, and the
+     * dimensions should be what your qfunction is expecting.
+     * One thing we can do is get the grayscale image
+     * where squares in the image are 0.0 if unoccupied, 0.5 if
+     * there is a "background" square (i.e. that square is occupied
+     * but it is not the current piece being placed), and 1.0 for
+     * any squares that the current piece is being considered for.
+     * 
+     * We can then flatten this image to get a row-vector, but we
+     * can do more than this! Try to be creative: how can you measure the
+     * "state" of the game without relying on the pixels? If you were given
+     * a tetris game midway through play, what properties would you look for?
      */
     @Override
     public Matrix getQFunctionInput(final GameView game,
-                                    final Mino potentialAction)
-    {
+            final Mino potentialAction) {
         Matrix flattenedImage = null;
         Matrix gameMatrix = null;
 
-        Matrix result = Matrix.zeros(1,12);
-        
+        Matrix result = Matrix.zeros(1, 12);
+
         // collect data
-        int num_holes_inside = 0; //empty spaces that have blocks above them
+        int num_holes_inside = 0; // empty spaces that have blocks above them
         int maxHeight = 0;
         int maxHeight_aft = 0;
         int maxHeight_bf = 0;
         int diff = 0;
         int holes = 0;
-        int bumpiness = 0; // the variance between the heights of adjacent columns, higher the variance, less opportunity to cancel out lines
-        int[] minoTypes = new int[]{0, 0, 0, 0, 0, 0, 0};
+        int bumpiness = 0; // the variance between the heights of adjacent columns, higher the variance,
+                           // less opportunity to cancel out lines
+        int[] minoTypes = new int[] { 0, 0, 0, 0, 0, 0, 0 };
         Board board = game.getBoard();
-        
-        try
-        {
+
+        try {
             flattenedImage = game.getGrayscaleImage(potentialAction).flatten();
             // get the grayscale image of the game board
             gameMatrix = game.getGrayscaleImage(potentialAction);
-        } catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
             System.exit(-1);
         }
-        //mino type set up
+        // mino type set up
         minoTypes[potentialAction.getType().ordinal()] = 1;
-        
-         //calculation
+
+        // calculation
         int previousHeight = -1;
-        for(int y = 0; y < gameMatrix.getShape().getNumCols(); y++){
+        for (int y = 0; y < gameMatrix.getShape().getNumCols(); y++) {
             int colHeight = 0;
             boolean found = false;
-            boolean goingto = false;//maximum height after I place current piece
+            boolean goingto = false;// maximum height after I place current piece
             int colHoles = 0;
             int currentheight = 0;
-            for(int x = 0; x < gameMatrix.getShape().getNumRows(); x++ ){
+            for (int x = 0; x < gameMatrix.getShape().getNumRows(); x++) {
                 Coordinate c = new Coordinate(x, y);
                 if (board.isInBounds(c) && board.isCoordinateOccupied(c)) {
                     found = true;
@@ -125,7 +120,7 @@ public class TetrisQAgent
                     if (maxHeight < colHeight) {
                         maxHeight = colHeight;
                     }
-                }else if (found) {
+                } else if (found) {
                     colHoles++;
                 }
 
@@ -146,12 +141,12 @@ public class TetrisQAgent
 
                 if (gameMatrix.get(y, x) == 0.0) {
                     // iterates numHoles when coordinate above is filled
-                    if (y > 0 && (gameMatrix.get(y-1, x) == 1.0 || gameMatrix.get(y-1, x) == 0.5)) {
+                    if (y > 0 && (gameMatrix.get(y - 1, x) == 1.0 || gameMatrix.get(y - 1, x) == 0.5)) {
                         num_holes_inside += 1;
                     }
                 }
 
-                //calculate bumpiness
+                // calculate bumpiness
                 if (gameMatrix.get(x, y) > 0) {
                     currentheight = Board.NUM_ROWS - x;
                     break;
@@ -166,10 +161,9 @@ public class TetrisQAgent
         int diff2 = maxHeight_bf - maxHeight_aft;
         if (diff2 <= 0) {
             diff = 0;
-        }
-        else {
+        } else {
             diff = diff2;
-        }    
+        }
         result.set(0, 0, maxHeight);
         result.set(0, 1, holes);
         result.set(0, 2, num_holes_inside);
@@ -187,25 +181,32 @@ public class TetrisQAgent
      * (i.e. our q-function), or if we should ignore it and take a random action
      * (i.e. explore).
      *
-     * Remember, as the q-function learns, it will start to predict the same "good" actions
-     * over and over again. This can prevent us from discovering new, potentially even
+     * Remember, as the q-function learns, it will start to predict the same "good"
+     * actions
+     * over and over again. This can prevent us from discovering new, potentially
+     * even
      * better states, which we want to do! So, sometimes we should ignore our policy
      * and explore to gain novel experiences.
      *
-     * The current implementation chooses to ignore the current policy around 5% of the time.
-     * While this strategy is easy to implement, it often doesn't perform well and is
+     * The current implementation chooses to ignore the current policy around 5% of
+     * the time.
+     * While this strategy is easy to implement, it often doesn't perform well and
+     * is
      * really sensitive to the EXPLORATION_PROB. I would recommend devising your own
      * strategy here.
      */
     @Override
     public boolean shouldExplore(final GameView game,
-                                 final GameCounter gameCounter)
-    {
-        return this.getRandom().nextDouble() <= EXPLORATION_PROB;
+            final GameCounter gameCounter) {
+        long t = gameCounter.getCurrentPhaseIdx();
+
+        return this.getRandom().nextDouble() <= (1 / (1 + t));
+        // return this.getRandom().nextDouble() <= EXPLORATION_PROB;
     }
 
     /**
-     * This method is a counterpart to the "shouldExplore" method. Whenever we decide
+     * This method is a counterpart to the "shouldExplore" method. Whenever we
+     * decide
      * that we should ignore our policy, we now have to actually choose an action.
      *
      * You should come up with a way of choosing an action so that the model gets
@@ -214,51 +215,55 @@ public class TetrisQAgent
      * I would recommend devising your own strategy here.
      */
     @Override
-    public Mino getExplorationMove(final GameView game)
-    {
+    public Mino getExplorationMove(final GameView game) {
         int randIdx = this.getRandom().nextInt(game.getFinalMinoPositions().size());
+
         return game.getFinalMinoPositions().get(randIdx);
     }
 
     /**
-     * This method is called by the TrainerAgent after we have played enough training games.
-     * In between the training section and the evaluation section of a phase, we need to use
-     * the exprience we've collected (from the training games) to improve the q-function.
+     * This method is called by the TrainerAgent after we have played enough
+     * training games.
+     * In between the training section and the evaluation section of a phase, we
+     * need to use
+     * the exprience we've collected (from the training games) to improve the
+     * q-function.
      *
-     * You don't really need to change this method unless you want to. All that happens
-     * is that we will use the experiences currently stored in the replay buffer to update
-     * our model. Updates (i.e. gradient descent updates) will be applied per minibatch
-     * (i.e. a subset of the entire dataset) rather than in a vanilla gradient descent manner
-     * (i.e. all at once)...this often works better and is an active area of research.
-     
-     * Each pass through the data is called an epoch, and we will perform "numUpdates" amount
+     * You don't really need to change this method unless you want to. All that
+     * happens
+     * is that we will use the experiences currently stored in the replay buffer to
+     * update
+     * our model. Updates (i.e. gradient descent updates) will be applied per
+     * minibatch
+     * (i.e. a subset of the entire dataset) rather than in a vanilla gradient
+     * descent manner
+     * (i.e. all at once)...this often works better and is an active area of
+     * research.
+     * 
+     * Each pass through the data is called an epoch, and we will perform
+     * "numUpdates" amount
      * of epochs in between the training and eval sections of each phase.
      */
     @Override
     public void trainQFunction(Dataset dataset,
-                               LossFunction lossFunction,
-                               Optimizer optimizer,
-                               long numUpdates)
-    {
-        for(int epochIdx = 0; epochIdx < numUpdates; ++epochIdx)
-        {
+            LossFunction lossFunction,
+            Optimizer optimizer,
+            long numUpdates) {
+        for (int epochIdx = 0; epochIdx < numUpdates; ++epochIdx) {
             dataset.shuffle();
-            Iterator<Pair<Matrix, Matrix> > batchIterator = dataset.iterator();
+            Iterator<Pair<Matrix, Matrix>> batchIterator = dataset.iterator();
 
-            while(batchIterator.hasNext())
-            {
+            while (batchIterator.hasNext()) {
                 Pair<Matrix, Matrix> batch = batchIterator.next();
 
-                try
-                {
+                try {
                     Matrix YHat = this.getQFunction().forward(batch.getFirst());
 
                     optimizer.reset();
                     this.getQFunction().backwards(batch.getFirst(),
-                                                  lossFunction.backwards(YHat, batch.getSecond()));
+                            lossFunction.backwards(YHat, batch.getSecond()));
                     optimizer.step();
-                } catch(Exception e)
-                {
+                } catch (Exception e) {
                     e.printStackTrace();
                     System.exit(-1);
                 }
@@ -267,52 +272,88 @@ public class TetrisQAgent
     }
 
     /**
-     * This method is where you will devise your own reward signal. Remember, the larger
-     * the number, the more "pleasurable" it is to the model, and the smaller the number,
+     * This method is where you will devise your own reward signal. Remember, the
+     * larger
+     * the number, the more "pleasurable" it is to the model, and the smaller the
+     * number,
      * the more "painful" to the model.
      *
      * This is where you get to tell the model how "good" or "bad" the game is.
-     * Since you earn points in this game, the reward should probably be influenced by the
-     * points, however this is not all. In fact, just using the points earned this turn
+     * Since you earn points in this game, the reward should probably be influenced
+     * by the
+     * points, however this is not all. In fact, just using the points earned this
+     * turn
      * is a **terrible** reward function, because earning points is hard!!
      *
-     * I would recommend you to consider other ways of measuring "good"ness and "bad"ness
-     * of the game. For instance, the higher the stack of minos gets....generally the worse
-     * (unless you have a long hole waiting for an I-block). When you design a reward
-     * signal that is less sparse, you should see your model optimize this reward over time.
+     * I would recommend you to consider other ways of measuring "good"ness and
+     * "bad"ness
+     * of the game. For instance, the higher the stack of minos gets....generally
+     * the worse
+     * (unless you have a long hole waiting for an I-block). When you design a
+     * reward
+     * signal that is less sparse, you should see your model optimize this reward
+     * over time.
      */
     @Override
-    public double getReward(final GameView game)
-    {
+    public double getReward(final GameView game) {
         Board board = game.getBoard();
         int numemptyb = 0;
         Coordinate highest = null;
         Boolean beginempty = false;
         double reward = 0.0;
 
-        for (int y = 0; y < Board.NUM_COLS; y++ ) {
+        for (int y = 0; y < Board.NUM_COLS; y++) {
+            beginempty = false;
             for (int x = 0; x < Board.NUM_ROWS; x++) {
                 Coordinate c = new Coordinate(x, y);
-                if (board.isInBounds(c) && board.isCoordinateOccupied(c) && !beginempty) { 
+                if (board.isInBounds(c) && board.isCoordinateOccupied(c) && !beginempty) {
                     // get the highest coordinate position
                     highest = c;
-                }
-                else if (board.isInBounds(c) && !(board.isCoordinateOccupied(x, y)) && beginempty) {
+                    beginempty = true;
+                } else if (board.isInBounds(c) && !(board.isCoordinateOccupied(x, y)) && beginempty) {
                     // number of occupied below highest
                     numemptyb += 1;
                 }
-            }if(highest!=null){
-                beginempty = true;
             }
         }
-       
+
+        int complete = 0;
+        for (int row = 0; row < Board.NUM_ROWS; row++) {
+            boolean completeLine = true;
+            for (int col = 0; col < Board.NUM_COLS; col++) {
+                Coordinate c = new Coordinate(row, col);
+                if (board.isInBounds(c) && !board.isCoordinateOccupied(c)) {
+                    completeLine = false;
+                    break;
+                }
+            }
+            if (completeLine) {
+                complete++;
+            }
+        }
+
+        int almost = 0;
+        for (int row = 0; row < Board.NUM_ROWS; row++) {
+            Double squares = 0d;
+            for (int col = 0; col < Board.NUM_COLS; col++) {
+                Coordinate c = new Coordinate(row, col);
+                if (board.isInBounds(c) && board.isCoordinateOccupied(c)) {
+                    squares++;
+                }
+            }
+            if (squares / Board.NUM_COLS >= 0.8) {
+                almost++;
+            }
+        }
+
         if (highest != null) {
-            double highestY = (Board.NUM_ROWS - highest.getYCoordinate());//higher the highest, smaller the highestY
-            //more height should minus more point
-            reward = (-1.0 / highestY) - 0.5*numemptyb + game.getScoreThisTurn();;//reward consider height and score
+            double highestY = (Board.NUM_ROWS - highest.getYCoordinate());// higher the highest, smaller the highestY
+            // more height should minus more point
+            reward = (-highestY) - 2 * numemptyb + 4 * almost + 10 * complete + game.getScoreThisTurn();
+            ;// reward consider height and score
             System.out.println("Reward value: " + reward + "num empty" + numemptyb);
         }
         return reward;
-    } 
+    }
 
 }
